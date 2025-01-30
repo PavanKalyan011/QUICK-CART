95f2fe9bd8c252913d46397255ad1ed86f8c9d65
pavankalayan011 <pavankalyanbaswaraju@gmail.comgit config --listgit config --global user.email pavankalyanbaswaraju@gmail.com>
2025-01-31 02:49:52 +0530

    Add build for deployment

--- src/databaseApi/Db_Instance.jsx
+++ src/databaseApi/Db_Instance.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import axios from 'axios'
+import React from 'react';
+import axios from 'axios';
 
-const Db_Instance =axios.create (
-    {
-    baseURL:"${process.env.REACT_APP_API_URL}/"
-}
-)
+const Db_Instance = axios.create({
+  baseURL: `${process.env.REACT_APP_API_URL}/`
+});
 
-export default Db_Instance
+export default Db_Instance;
